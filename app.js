2af5cd2ac8ebd722e2858fb6f2a0c36c9f2c4efe
InesMiranda <33327687+InesMiranda@users.noreply.github.com>
2019-04-17 16:58:04 +0100

    Added live data for weather

--- app.js
+++ app.js
@@ -10,10 +10,23 @@ let humidity = document.querySelector("#weather-humidity");
 let visibility = document.querySelector("#weather-visibility");
 let uvIndex = document.querySelector("#weather-uv-index");
 
+let apiKey = "ea123f7f053ae1c00499328f0f8b0c1c";
+let apiRoot = "https://api.openweathermap.org/data/2.5"
+let apiUrl = `${apiRoot}/weather?q=Lisbon&appid=${apiKey}&units=metric`;
 
-city.innerHTML = "Sydney";
-country.innerHTML = "Spain";
-time.innerHTML = "Wednesday 17/04";
-description.innerHTML = "Sol";
-temperature.innerHTML = 20;
-humidity.innerHTML = 10;
+
+function refreshWeather(response){
+    city.innerHTML = response.data.name;
+    country.innerHTML = response.data.sys.country;
+    time.innerHTML = response.data.dt;
+    description.innerHTML = response.data.weather[0].main;
+    temperature.innerHTML = response.data.main.temp;
+    humidity.innerHTML = response.data.main.humidity;
+    windSpeed.innerHTML = response.data.wind.speed;
+    temperatureHigher.innerHTML = response.data.main.temp_max;
+    temperatureLower.innerHTML = response.data.main.temp_min;
+    visibility.innerHTML = response.data.visibility;
+}
+
+axios.get(apiUrl).then(refreshWeather);
+
