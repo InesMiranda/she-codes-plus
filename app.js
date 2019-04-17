b5e01bac49bd831e35ff1e80a2dcaba50a717a10
InesMiranda <33327687+InesMiranda@users.noreply.github.com>
2019-04-17 16:30:45 +0100

    Moved weather data to JS

---
+++ app.js
@@ -0,0 +1,18 @@
+let city = document.querySelector("#city");
+let country = document.querySelector("#country");
+let time = document.querySelector("#date-time");
+let description = document.querySelector("#weather-description");
+let temperature = document.querySelector("#weather-temperature");
+let temperatureHigher = document.querySelector("#weather-higher-temperature");
+let temperatureLower = document.querySelector("#weather-lower-temperature");
+let humidity = document.querySelector("#weather-humidity");
+let visibility = document.querySelector("#weather-visibility");
+let uvIndex = document.querySelector("#weather-uv-index");
+
+
+city.innerHTML = "Sydney";
+country.innerHTML = "Spain";
+time.innerHTML = "Wednesday 17/04";
+description.innerHTML = "Sol";
+temperature.innerHTML = 20;
+humidity.innerHTML = 10;
